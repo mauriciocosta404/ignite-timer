0344d80a7f5d4ee027d599cd6dbb817da32ada21
mauriciocosta404 <mc611641@gmail.com>
2024-09-18 19:36:02 +0100

    feat: começando o historico

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,5 +1,43 @@
 import React from "react"
+import * as S from "./styles"
 
 export function History () {
-    return (<>History</>)
+    return (
+        <S.HistoryContainer>
+            <h1>Meu histórico</h1>
+
+            <S.HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>Concluido</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>Concluido</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>Concluido</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </S.HistoryList>
+        </S.HistoryContainer>
+    )
 }
